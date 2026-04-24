38ad17794226b01b426f14ea33a68acc5c11ace1
Matheus <matheus.werneck2006@gmail.com>
2026-04-24 16:50:25 -0700

    box resize and edited some stuff

--- Scripts/dialogue_talk.gd
+++ Scripts/dialogue_talk.gd
@@ -4,7 +4,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Dialogic.current_timeline != null:
+		return
+	Dialogic.start(dt)
+	get_viewport().set_input_as_handled()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
